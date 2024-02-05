a52b84316b575f0a193b1baad821c5aaf6fa3cc9
Ladislav Stárek <ladislav.starek@Ladislavs-MacBook-Pro.local>
2024-02-05 03:06:27 +0100

    Initialize project using Create React App

---
+++ src/CurrencyTable/CurrencyTable.tsx
@@ -0,0 +1,11 @@
+import React, {FC, memo} from 'react';
+
+const BareCurrencyTable:FC = () => {
+  return (
+    <div>
+      content
+    </div>
+  );
+}
+
+export const CurrencyTable = memo(BareCurrencyTable);

43c99e7555a819ef72113b5853764a3cba18b6f6
Ladislav Stárek <ladislav.starek@Ladislavs-MacBook-Pro.local>
2024-02-05 14:53:50 +0100

    Show error message when data load fails

--- src/CurrencyTable/CurrencyTable.tsx
+++ src/CurrencyTable/CurrencyTable.tsx
@@ -1,6 +1,7 @@
 import React, {FC, memo} from 'react';
 import {useTableData} from './useTableData';
 import {SpinnerWrapper, Spinner} from './Spinner';
+import {ErrorMessage} from './ErrorMessage';
 
 const BareCurrencyTable:FC = () => {
   const {isLoading, data, error} = useTableData();
@@ -10,6 +11,14 @@ const BareCurrencyTable:FC = () => {
       <SpinnerWrapper>
         <Spinner />
       </SpinnerWrapper>
+    );
+  }
+
+  if (error != null) {
+    return (
+      <ErrorMessage>
+        {error.message ?? 'Data load failed'}
+      </ErrorMessage>
     );
   }
 
